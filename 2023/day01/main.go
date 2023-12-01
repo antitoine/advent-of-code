22a44abfe9eac3468bfeceaccafe3f590734fec9
Antoine CHABERT <chabert.antoine@gmail.com>
2023-12-01 14:23:05 +0100

    2023-Day-01 Part 2

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -8,8 +8,75 @@ import (
 	"strings"
 )
 
+var digitAsLetters = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func isDigit(r rune) bool {
 	return r >= '0' && r <= '9'
+}
+
+func getFirstDigitFromString(line string) string {
+	for i, r := range []rune(line) {
+		if isDigit(r) {
+			return string(r)
+		}
+		for letters, digit := range digitAsLetters {
+			if strings.HasPrefix(line[i:], letters) {
+				return digit
+			}
+		}
+	}
+	log.Fatalf("Unable to get first digit from provided line '%s'", line)
+	return "0"
+}
+
+func getLastDigitFromString(line string) string {
+	for i := len(line) - 1; i >= 0; i-- {
+		r := rune(line[i])
+		if isDigit(r) {
+			return string(r)
+		}
+		for letters, digit := range digitAsLetters {
+			if strings.HasSuffix(line[:i+1], letters) {
+				return digit
+			}
+		}
+	}
+	log.Fatalf("Unable to get last digit from provided line '%s'", line)
+	return "0"
+}
+
+func getDigitsFromString(line string) int64 {
+	firstDigit := getFirstDigitFromString(line)
+	lastDigit := getLastDigitFromString(line)
+	number, errParsing := strconv.ParseInt(firstDigit+lastDigit, 10, 64)
+	if errParsing != nil {
+		log.Fatalf("Unable to parse digits provided '%s' & '%s': %v", firstDigit, lastDigit, errParsing)
+	}
+	return number
+}
+
+func getSumOfDigits(file *os.File) int64 {
+	var finalSum int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		finalSum += getDigitsFromString(scanner.Text())
+	}
+
+	if errScanningFile := scanner.Err(); errScanningFile != nil {
+		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
+	}
+
+	return finalSum
 }
 
 func main() {
@@ -19,29 +86,7 @@ func main() {
 	}
 	defer inputFile.Close()
 
-	var finalSum int64
-	scanner := bufio.NewScanner(inputFile)
-	for scanner.Scan() {
-		textLine := scanner.Text()
-		firstDigitIdx := strings.IndexFunc(textLine, isDigit)
-		if firstDigitIdx < 0 {
-			log.Fatalf("Unable to find the first digit in the line: %v", textLine)
-		}
-		LastDigitIdx := strings.LastIndexFunc(textLine, isDigit)
-		if LastDigitIdx < 0 {
-			log.Fatalf("Unable to find the last digit in the line: %v", textLine)
-		}
-		textLineRunes := []rune(textLine)
-		lineNumber, errParsing := strconv.ParseInt(string(textLineRunes[firstDigitIdx])+string(textLineRunes[LastDigitIdx]), 10, 64)
-		if errParsing != nil {
-			log.Fatalf("Unable to parse the line number (%v with %v) in line text '%s': %v", textLine[firstDigitIdx], textLine[LastDigitIdx], textLine, errParsing)
-		}
-		finalSum += lineNumber
-	}
-
-	if errScanningFile := scanner.Err(); errScanningFile != nil {
-		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
-	}
+	finalSum := getSumOfDigits(inputFile)
 
 	log.Printf("Final sum: %d", finalSum)
 }
