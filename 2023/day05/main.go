1e9d9ff46d333e18dde670702dca93974777b097
Antoine CHABERT <chabert.antoine@gmail.com>
2023-12-05 09:44:45 +0100

    2023-Day-05 Part 1

---
+++ 2023/day05/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+type RangeMap struct {
+	from int64
+	to   int64
+	len  int64
+}
+
+type Map struct {
+	from    string
+	to      string
+	mapping []RangeMap
+	nextMap *Map
+}
+
+var numbersRegex = regexp.MustCompile(`\s*(\d*)\s*`)
+
+func numbersStrToInts(numbersStr string) []int64 {
+	var numbers []int64
+	for _, numberStr := range numbersRegex.FindAllStringSubmatch(numbersStr, -1) {
+		if len(numberStr) != 2 {
+			log.Fatalf("Unable to parse number '%s', getting results length of %d", numbersStr, len(numberStr))
+		}
+		numberInt, errParsingNumber := strconv.ParseInt(numberStr[1], 10, 64)
+		if errParsingNumber != nil {
+			log.Fatalf("Unable to parse number '%s': %v", numberStr[1], errParsingNumber)
+		}
+		numbers = append(numbers, numberInt)
+	}
+	return numbers
+}
+
+var seedsLineRegex = regexp.MustCompile(`^seeds: (.*)$`)
+
+func parseSeeds(line string) []int64 {
+	results := seedsLineRegex.FindStringSubmatch(line)
+	if len(results) != 2 {
+		log.Fatalf("Unable to parse seeds line '%s', getting results length of %d", line, len(results))
+	}
+	return numbersStrToInts(results[1])
+}
+
+var mappingLineRegex = regexp.MustCompile(`^([^-]*)-to-([^ ]*) map:$`)
+
+func parseMapping(line string) (string, string) {
+	results := mappingLineRegex.FindStringSubmatch(line)
+	if len(results) != 3 {
+		log.Fatalf("Unable to parse mapping line '%s', getting results length of %d", line, len(results))
+	}
+	return results[1], results[2]
+}
+
+func parseInput(input io.Reader) ([]int64, *Map) {
+	scanner := bufio.NewScanner(input)
+
+	// Seeds
+	if !scanner.Scan() {
+		log.Fatalf("Unable to scan the first line of the input correctly: %v", scanner.Err())
+	}
+	seeds := parseSeeds(scanner.Text())
+
+	scanner.Scan() // Empty line
+
+	// Mapping
+	var firstMap *Map
+	var lastMap *Map
+	for scanner.Scan() {
+		from, to := parseMapping(scanner.Text())
+		newMap := &Map{
+			from: from,
+			to:   to,
+		}
+		if firstMap == nil {
+			firstMap = newMap
+		}
+		if lastMap != nil {
+			lastMap.nextMap = newMap
+		}
+		for scanner.Scan() {
+			mappingRangeStr := scanner.Text()
+			if mappingRangeStr == "" {
+				break
+			}
+			mappingRange := numbersStrToInts(mappingRangeStr)
+			if len(mappingRange) != 3 {
+				log.Fatalf("Unable to parse mapping range '%s', getting results length of %d", mappingRangeStr, len(mappingRange))
+			}
+			newRangeMap := RangeMap{
+				from: mappingRange[1],
+				to:   mappingRange[0],
+				len:  mappingRange[2],
+			}
+			newMap.mapping = append(newMap.mapping, newRangeMap)
+		}
+		sort.Slice(newMap.mapping, func(i, j int) bool { return newMap.mapping[i].from < newMap.mapping[j].from })
+		lastMap = newMap
+	}
+
+	if errScanningFile := scanner.Err(); errScanningFile != nil {
+		log.Fatalf("Unable to scan the input correctly: %v", errScanningFile)
+	}
+
+	return seeds, firstMap
+}
+
+func getLowestLocation(input io.Reader) int64 {
+	seeds, firstMap := parseInput(input)
+
+	lowestLocation := int64(-1)
+	for _, seed := range seeds {
+		currentMap := firstMap
+		currentIdx := seed
+		for {
+			for _, mapping := range currentMap.mapping {
+				if currentIdx >= mapping.from && currentIdx < mapping.from+mapping.len {
+					currentIdx = (currentIdx - mapping.from) + mapping.to
+					break
+				}
+			}
+			if currentMap.nextMap != nil {
+				currentMap = currentMap.nextMap
+			} else {
+				break
+			}
+		}
+		if lowestLocation == -1 || currentIdx < lowestLocation {
+			lowestLocation = currentIdx
+		}
+	}
+
+	return lowestLocation
+}
+
+func main() {
+	inputFile, errOpeningFile := os.Open("./input.txt")
+	if errOpeningFile != nil {
+		log.Fatalf("Unable to open input file: %v", errOpeningFile)
+	}
+	defer inputFile.Close()
+
+	lowestLocation := getLowestLocation(inputFile)
+
+	log.Printf("Final lowest location: %d", lowestLocation)
+}
