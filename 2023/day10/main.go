63ec2f4fa375269aa96af30b6c9dd984cfdd1cd2
Antoine CHABERT <chabert.antoine@gmail.com>
2023-12-10 16:11:50 +0100

    2023-Day-10 Part 2

--- 2023/day10/main.go
+++ 2023/day10/main.go
@@ -15,18 +15,8 @@ type Node struct {
 	j              int
 	symbol         rune
 	connectedNodes []*Node
+	isLoopNode     bool
 }
-
-/*
-   | is a vertical pipe connecting north and south.
-   - is a horizontal pipe connecting east and west.
-   L is a 90-degree bend connecting north and east.
-   J is a 90-degree bend connecting north and west.
-   7 is a 90-degree bend connecting south and west.
-   F is a 90-degree bend connecting south and east.
-   . is ground; there is no pipe in this tile.
-   S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-*/
 
 func (n *Node) isConnectedToNode(otherNode *Node) bool {
 	if n == nil || otherNode == nil {
@@ -40,72 +30,73 @@ func (n *Node) isConnectedToNode(otherNode *Node) bool {
 	return false
 }
 
-func parseInput(input io.Reader) *Node {
+func parseInput(input io.Reader) ([][]*Node, *Node) {
 	scanner := bufio.NewScanner(input)
-	var draftGraph []*Node
-	nodes := make([][]*Node, mapSize)
+	var nodes []*Node
+	var graph [][]*Node
 	var startingNode *Node
 	for i := 0; scanner.Scan(); i++ {
-		nodes[i] = make([]*Node, mapSize)
-		for j, symbol := range scanner.Text() {
+		line := scanner.Text()
+		lineNodes := make([]*Node, len(line))
+		for j, symbol := range line {
 			node := &Node{
-				i:              i,
-				j:              j,
-				symbol:         symbol,
-				connectedNodes: make([]*Node, 2),
+				i:      i,
+				j:      j,
+				symbol: symbol,
 			}
-			draftGraph = append(draftGraph, node)
-			nodes[i][j] = node
+			lineNodes[j] = node
+			nodes = append(nodes, node)
 		}
+		graph = append(graph, lineNodes)
 	}
 	if errScanningFile := scanner.Err(); errScanningFile != nil {
 		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
 	}
 
-	for _, node := range draftGraph {
+	for _, node := range nodes {
 		switch node.symbol {
 		case '.':
 			continue
 		case '|':
-			if node.i > 0 && node.i < mapSize-1 {
-				node.connectedNodes[0] = nodes[node.i-1][node.j]
-				node.connectedNodes[1] = nodes[node.i+1][node.j]
+			if node.i > 0 && node.i < len(graph)-1 {
+				node.connectedNodes = append(node.connectedNodes, graph[node.i-1][node.j], graph[node.i+1][node.j])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case '-':
-			if node.j > 0 && node.j < mapSize-1 {
-				node.connectedNodes[0] = nodes[node.i][node.j-1]
-				node.connectedNodes[1] = nodes[node.i][node.j+1]
+			if node.j > 0 && node.j < len(graph[node.i])-1 {
+				node.connectedNodes = append(node.connectedNodes, graph[node.i][node.j-1], graph[node.i][node.j+1])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case 'L':
-			if node.i > 0 && node.j < mapSize-1 {
-				node.connectedNodes[0] = nodes[node.i-1][node.j]
-				node.connectedNodes[1] = nodes[node.i][node.j+1]
+			if node.i > 0 && node.j < len(graph[node.i])-1 {
+				node.connectedNodes = append(node.connectedNodes, graph[node.i-1][node.j], graph[node.i][node.j+1])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case 'J':
 			if node.i > 0 && node.j > 0 {
-				node.connectedNodes[0] = nodes[node.i-1][node.j]
-				node.connectedNodes[1] = nodes[node.i][node.j-1]
+				node.connectedNodes = append(node.connectedNodes, graph[node.i-1][node.j], graph[node.i][node.j-1])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case '7':
-			if node.i < mapSize-1 && node.j > 0 {
-				node.connectedNodes[0] = nodes[node.i+1][node.j]
-				node.connectedNodes[1] = nodes[node.i][node.j-1]
+			if node.i < len(graph)-1 && node.j > 0 {
+				node.connectedNodes = append(node.connectedNodes, graph[node.i+1][node.j], graph[node.i][node.j-1])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case 'F':
-			if node.i < mapSize-1 && node.j < mapSize-1 {
-				node.connectedNodes[0] = nodes[node.i+1][node.j]
-				node.connectedNodes[1] = nodes[node.i][node.j+1]
+			if node.i < len(graph)-1 && node.j < len(graph[node.i])-1 {
+				node.connectedNodes = append(node.connectedNodes, graph[node.i+1][node.j], graph[node.i][node.j+1])
 			} else {
+				node.symbol = '.'
 				continue
 			}
 		case 'S':
@@ -118,51 +109,140 @@ func parseInput(input io.Reader) *Node {
 	if startingNode == nil {
 		log.Fatalf("No starting node found")
 	} else {
-		connectedNodesIdx := 0
-		if startingNode.i > 0 && nodes[startingNode.i-1][startingNode.j].isConnectedToNode(startingNode) {
-			startingNode.connectedNodes[connectedNodesIdx] = nodes[startingNode.i-1][startingNode.j]
-			connectedNodesIdx++
+		fromNorth := false
+		fromSouth := false
+		fromEast := false
+		fromWest := false
+		if startingNode.i > 0 && graph[startingNode.i-1][startingNode.j].isConnectedToNode(startingNode) {
+			startingNode.connectedNodes = append(startingNode.connectedNodes, graph[startingNode.i-1][startingNode.j])
+			fromNorth = true
 		}
-		if startingNode.i < mapSize-1 && nodes[startingNode.i+1][startingNode.j].isConnectedToNode(startingNode) {
-			startingNode.connectedNodes[connectedNodesIdx] = nodes[startingNode.i+1][startingNode.j]
-			connectedNodesIdx++
+		if startingNode.i < len(graph)-1 && graph[startingNode.i+1][startingNode.j].isConnectedToNode(startingNode) {
+			startingNode.connectedNodes = append(startingNode.connectedNodes, graph[startingNode.i+1][startingNode.j])
+			fromSouth = true
 		}
-		if startingNode.j > 0 && nodes[startingNode.i][startingNode.j-1].isConnectedToNode(startingNode) {
-			startingNode.connectedNodes[connectedNodesIdx] = nodes[startingNode.i][startingNode.j-1]
-			connectedNodesIdx++
+		if startingNode.j > 0 && graph[startingNode.i][startingNode.j-1].isConnectedToNode(startingNode) {
+			startingNode.connectedNodes = append(startingNode.connectedNodes, graph[startingNode.i][startingNode.j-1])
+			fromWest = true
 		}
-		if startingNode.j < mapSize-1 && nodes[startingNode.i][startingNode.j+1].isConnectedToNode(startingNode) {
-			startingNode.connectedNodes[connectedNodesIdx] = nodes[startingNode.i][startingNode.j+1]
-			connectedNodesIdx++
+		if startingNode.j < len(graph[startingNode.i])-1 && graph[startingNode.i][startingNode.j+1].isConnectedToNode(startingNode) {
+			startingNode.connectedNodes = append(startingNode.connectedNodes, graph[startingNode.i][startingNode.j+1])
+			fromEast = true
 		}
-		if connectedNodesIdx != 2 {
-			log.Fatalf("Starting node has %d connected nodes", connectedNodesIdx)
+		if len(startingNode.connectedNodes) != 2 {
+			log.Fatalf("Starting node has %d connected nodes", len(startingNode.connectedNodes))
+		}
+
+		if fromNorth && fromSouth && !fromEast && !fromWest {
+			startingNode.symbol = '|'
+		} else if !fromNorth && !fromSouth && fromEast && fromWest {
+			startingNode.symbol = '-'
+		} else if fromNorth && !fromSouth && fromEast && !fromWest {
+			startingNode.symbol = 'L'
+		} else if fromNorth && !fromSouth && !fromEast && fromWest {
+			startingNode.symbol = 'J'
+		} else if !fromNorth && fromSouth && !fromEast && fromWest {
+			startingNode.symbol = '7'
+		} else if !fromNorth && fromSouth && fromEast && !fromWest {
+			startingNode.symbol = 'F'
+		} else {
+			log.Fatalf("Unable to determine starting node symbol: %#v", startingNode)
 		}
 	}
 
-	return startingNode
+	return graph, startingNode
 }
 
-func getResult(input io.Reader) int64 {
-	startingNode := parseInput(input)
+func printGraph(graph [][]*Node) {
+	for _, line := range graph {
+		lineStr := ""
+		for _, node := range line {
+			lineStr += string(node.symbol)
+		}
+		log.Printf("%s", lineStr)
+	}
+}
+
+func navigate(startingNode *Node) int64 {
 	prevNode := startingNode
 	currentNode := startingNode.connectedNodes[0]
+	currentNode.isLoopNode = true
 	var step int64
 	for step = 1; currentNode != startingNode; step++ {
-		if currentNode.connectedNodes[0] != prevNode {
-			prevNode = currentNode
-			currentNode = currentNode.connectedNodes[0]
-		} else if currentNode.connectedNodes[1] != prevNode {
-			prevNode = currentNode
-			currentNode = currentNode.connectedNodes[1]
-		} else {
+		var c int
+		for c = 0; c < len(currentNode.connectedNodes) && currentNode.connectedNodes[c] == prevNode; c++ {
+		}
+		if c == len(currentNode.connectedNodes) {
 			log.Fatalf("Unable to find next node from %v", currentNode)
 		}
+		prevNode = currentNode
+		currentNode = currentNode.connectedNodes[c]
+		currentNode.isLoopNode = true
 	}
+	return step
+}
+
+func getResultPart1(input io.Reader) int64 {
+	graph, startingNode := parseInput(input)
+	printGraph(graph)
+	log.Printf("Starting node at i=%d j=%d", startingNode.i, startingNode.j)
+	step := navigate(startingNode)
 	if step%2 != 0 {
 		log.Fatalf("Step is not even: %d", step)
 	}
 	return step / 2
+}
+
+func getResultPart2(input io.Reader) int64 {
+	graph, startingNode := parseInput(input)
+	printGraph(graph)
+	log.Printf("Starting node at i=%d j=%d", startingNode.i, startingNode.j)
+	navigate(startingNode)
+	var nodesInsideLoop int64
+	for i, line := range graph {
+		if i == 0 || i == len(graph)-1 {
+			continue
+		}
+		for j, node := range line {
+			if j == 0 || j == len(line)-1 || node.isLoopNode {
+				continue
+			}
+			var loopIntersection int64
+			for expJ := j - 1; expJ >= 0; expJ-- {
+				if graph[i][expJ].isLoopNode {
+					if graph[i][expJ].symbol == '|' {
+						loopIntersection++
+					} else if graph[i][expJ].symbol == 'J' {
+						expJ--
+						for expJ >= 0 && graph[i][expJ].symbol == '-' {
+							expJ--
+						}
+						if expJ < 0 {
+							log.Fatalf("Unable to find node before J")
+						}
+						if graph[i][expJ].symbol == 'F' {
+							loopIntersection++
+						}
+					} else if graph[i][expJ].symbol == '7' {
+						expJ--
+						for expJ >= 0 && graph[i][expJ].symbol == '-' {
+							expJ--
+						}
+						if expJ < 0 {
+							log.Fatalf("Unable to find node before J")
+						}
+						if graph[i][expJ].symbol == 'L' {
+							loopIntersection++
+						}
+					}
+				}
+			}
+			if loopIntersection%2 == 1 {
+				nodesInsideLoop++
+			}
+		}
+	}
+	return nodesInsideLoop
 }
 
 func loadFile() *os.File {
@@ -178,7 +258,7 @@ func main() {
 	inputFile := loadFile()
 	defer inputFile.Close()
 
-	result := getResult(inputFile)
+	result := getResultPart2(inputFile)
 
 	log.Printf("Final result: %d", result)
 	log.Printf("Execution took %s", time.Since(start))
--- 2023/day10/main_test.go
+++ 2023/day10/main_test.go
@@ -6,54 +6,143 @@ import (
 )
 
 func TestGetResults(t *testing.T) {
-	t.Run("example 1", func(t *testing.T) {
-		input := `.....
+	t.Run("part 1", func(t *testing.T) {
+		t.Run("example 1", func(t *testing.T) {
+			input := `.....
 .S-7.
 .|.|.
 .L-J.
 .....
 `
 
-		result := getResult(strings.NewReader(input))
-		if result != 4 {
-			t.Errorf("Expected result to be 4, got %d", result)
-		}
-	})
-	t.Run("example 2", func(t *testing.T) {
-		input := `..F7.
+			result := getResultPart1(strings.NewReader(input))
+			if result != 4 {
+				t.Errorf("Expected result to be 4, got %d", result)
+			}
+		})
+		t.Run("example 2", func(t *testing.T) {
+			input := `..F7.
 .FJ|.
 SJ.L7
 |F--J
 LJ...
 `
 
-		result := getResult(strings.NewReader(input))
-		if result != 8 {
-			t.Errorf("Expected result to be 8, got %d", result)
-		}
+			result := getResultPart1(strings.NewReader(input))
+			if result != 8 {
+				t.Errorf("Expected result to be 8, got %d", result)
+			}
+		})
+	})
+
+	t.Run("part 2", func(t *testing.T) {
+		t.Run("example 1", func(t *testing.T) {
+			input := `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+`
+
+			result := getResultPart2(strings.NewReader(input))
+			if result != 4 {
+				t.Errorf("Expected result to be 4, got %d", result)
+			}
+		})
+		t.Run("example 2", func(t *testing.T) {
+			input := `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+`
+
+			result := getResultPart2(strings.NewReader(input))
+			if result != 8 {
+				t.Errorf("Expected result to be 8, got %d", result)
+			}
+		})
+		t.Run("example 3", func(t *testing.T) {
+			input := `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+`
+
+			result := getResultPart2(strings.NewReader(input))
+			if result != 10 {
+				t.Errorf("Expected result to be 10, got %d", result)
+			}
+		})
 	})
 }
 
 func BenchmarkGetResult(b *testing.B) {
-	b.Run("small", func(b *testing.B) {
-		input := `..F7.
+	b.Run("part 1", func(b *testing.B) {
+		b.Run("small", func(b *testing.B) {
+			input := `..F7.
 .FJ|.
 SJ.L7
 |F--J
 LJ...
 `
 
-		for n := 0; n < b.N; n++ {
-			getResult(strings.NewReader(input))
-		}
+			for n := 0; n < b.N; n++ {
+				getResultPart1(strings.NewReader(input))
+			}
+		})
+
+		b.Run("large", func(b *testing.B) {
+			inputFile := loadFile()
+			defer inputFile.Close()
+
+			for n := 0; n < b.N; n++ {
+				getResultPart1(inputFile)
+			}
+		})
 	})
 
-	b.Run("large", func(b *testing.B) {
-		inputFile := loadFile()
-		defer inputFile.Close()
+	b.Run("part 2", func(b *testing.B) {
+		b.Run("small", func(b *testing.B) {
+			input := `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+`
 
-		for n := 0; n < b.N; n++ {
-			getResult(inputFile)
-		}
+			for n := 0; n < b.N; n++ {
+				getResultPart2(strings.NewReader(input))
+			}
+		})
+
+		b.Run("large", func(b *testing.B) {
+			inputFile := loadFile()
+			defer inputFile.Close()
+
+			for n := 0; n < b.N; n++ {
+				getResultPart2(inputFile)
+			}
+		})
 	})
 }
