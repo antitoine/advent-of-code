b5196cb4d1aef3664afce94d557c0452272039f7
Antoine CHABERT <chabert.antoine@gmail.com>
2023-12-18 10:54:28 +0100

    2023-Day-18 Part 1

---
+++ 2023/day18/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type Grid [][]rune
+
+type Direction int
+
+const (
+	Up    Direction = iota
+	Down  Direction = iota
+	Left  Direction = iota
+	Right Direction = iota
+)
+
+type Position struct {
+	i, j int64
+}
+
+func (p Position) Move(direction Direction) Position {
+	switch direction {
+	case Up:
+		return Position{p.i - 1, p.j}
+	case Down:
+		return Position{p.i + 1, p.j}
+	case Left:
+		return Position{p.i, p.j - 1}
+	case Right:
+		return Position{p.i, p.j + 1}
+	}
+	log.Fatalf("Unknown direction: %d", direction)
+	return p
+}
+
+var lineRegex = regexp.MustCompile(`^([UDLR]) (\d+) \((#[0-9a-f]{6})\)$`)
+
+func parseLine(line string) (Direction, int64, string) {
+	matches := lineRegex.FindStringSubmatch(line)
+	if matches == nil || len(matches) != 4 {
+		log.Fatalf("Unable to parse line: %s", line)
+	}
+
+	directionStr := matches[1]
+	var direction Direction
+	switch directionStr {
+	case "U":
+		direction = Up
+	case "D":
+		direction = Down
+	case "L":
+		direction = Left
+	case "R":
+		direction = Right
+	}
+	distance, errParsingDistance := strconv.ParseInt(matches[2], 10, 64)
+	if errParsingDistance != nil {
+		log.Fatalf("Unable to parse distance: %s", matches[2])
+	}
+	color := matches[3]
+	return direction, distance, color
+}
+
+func parseInput(input io.Reader) ([]Position, map[Position]string, int64) {
+	scanner := bufio.NewScanner(input)
+
+	var vertices []Position
+	colors := make(map[Position]string)
+	currentPosition := Position{0, 0}
+	vertices = append(vertices, currentPosition)
+	var boundaryPoints int64
+	for scanner.Scan() {
+		direction, distance, color := parseLine(scanner.Text())
+		boundaryPoints += distance
+		for i := int64(0); i < distance; i++ {
+			if _, alreadyDug := colors[currentPosition]; !alreadyDug {
+				colors[currentPosition] = color
+			}
+			newPosition := currentPosition.Move(direction)
+			colors[newPosition] = color
+			currentPosition = newPosition
+		}
+		vertices = append(vertices, currentPosition)
+	}
+
+	if errScanningFile := scanner.Err(); errScanningFile != nil {
+		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
+	}
+
+	return vertices, colors, boundaryPoints
+}
+
+func getResult(input io.Reader) int64 {
+	vertices, _, boundaryPoints := parseInput(input)
+
+	// Compute area using the shoelace formula
+	// https://en.wikipedia.org/wiki/Shoelace_formula
+	var area int64
+	for i := int64(0); i < int64(len(vertices))-1; i++ {
+		pointA := vertices[i]
+		pointB := vertices[i+1]
+		area += (pointA.j * pointB.i) - (pointA.i * pointB.j)
+	}
+	area /= 2
+
+	// Computed points
+	// https://en.wikipedia.org/wiki/Pick%27s_theorem
+	return area - (boundaryPoints / 2) + 1 + boundaryPoints
+}
+
+func loadFile() *os.File {
+	inputFile, errOpeningFile := os.Open("./input.txt")
+	if errOpeningFile != nil {
+		log.Fatalf("Unable to open input file: %v", errOpeningFile)
+	}
+	return inputFile
+}
+
+func main() {
+	start := time.Now()
+	inputFile := loadFile()
+	defer inputFile.Close()
+
+	result := getResult(inputFile)
+
+	log.Printf("Final result: %d", result)
+	log.Printf("Execution took %s", time.Since(start))
+}
---
+++ 2023/day18/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+const testingInput = `R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+`
+
+const testingExpectedResult = 62
+
+func TestGetResults(t *testing.T) {
+	result := getResult(strings.NewReader(testingInput))
+	if result != testingExpectedResult {
+		t.Errorf("Expected result to be %d, got %d", testingExpectedResult, result)
+	}
+}
+
+func BenchmarkGetResult(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			getResult(strings.NewReader(testingInput))
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			inputFile := loadFile()
+			getResult(inputFile)
+			inputFile.Close()
+		}
+	})
+}
