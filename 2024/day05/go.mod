f215c978e8780e28c0a2fd72afa5021d130c069a
Antoine CHABERT <chabert.antoine@gmail.com>
2024-12-05 08:19:09 +0100

    2024-Day-05 Part 1

---
+++ 2024/day05/go.mod
@@ -0,0 +1,3 @@
+module github.com/antitoine/advent-of-code/2024/day05
+
+go 1.23.2
---
+++ 2024/day05/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func parseRule(line string) (int, int) {
+	parts := strings.Split(line, "|")
+	if len(parts) != 2 {
+		log.Fatalf("Invalid rule: %s", line)
+	}
+	before, errBefore := strconv.Atoi(parts[0])
+	if errBefore != nil {
+		log.Fatalf("Invalid rule for before number: %s", line)
+	}
+	after, errAfter := strconv.Atoi(parts[1])
+	if errAfter != nil {
+		log.Fatalf("Invalid rule for after number: %s", line)
+	}
+	return before, after
+}
+
+func parseUpdate(line string) []int {
+	var updates []int
+	parts := strings.Split(line, ",")
+	for _, part := range parts {
+		num, err := strconv.Atoi(part)
+		if err != nil {
+			log.Fatalf("Invalid update number: %s", line)
+		}
+		updates = append(updates, num)
+	}
+	return updates
+}
+
+func parseInput(input io.Reader) (map[int][]int, [][]int) {
+	scanner := bufio.NewScanner(input)
+
+	rules := make(map[int][]int)
+	var updates [][]int
+	switchToUpdates := false
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			switchToUpdates = true
+			continue
+		}
+		if !switchToUpdates {
+			before, after := parseRule(text)
+			rules[before] = append(rules[before], after)
+		} else {
+			updates = append(updates, parseUpdate(text))
+		}
+	}
+
+	if errScanningFile := scanner.Err(); errScanningFile != nil {
+		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
+	}
+
+	return rules, updates
+}
+
+func getResult(input io.Reader) int64 {
+	rules, updates := parseInput(input)
+
+	var result int64
+	for _, update := range updates {
+		valid := true
+		process := make(map[int]struct{})
+		for _, nb := range update {
+			if rule, found := rules[nb]; found {
+				for _, after := range rule {
+					if _, foundAfter := process[after]; foundAfter {
+						valid = false
+						break
+					}
+				}
+			}
+			if !valid {
+				break
+			}
+			process[nb] = struct{}{}
+		}
+		if valid {
+			middlePage := update[len(update)/2]
+			result += int64(middlePage)
+		}
+	}
+
+	return result
+}
+
+func loadFile() *os.File {
+	inputFile, errOpeningFile := os.Open("./input.txt")
+	if errOpeningFile != nil {
+		log.Fatalf("Unable to open input file: %v", errOpeningFile)
+	}
+	return inputFile
+}
+
+func main() {
+	start := time.Now()
+	inputFile := loadFile()
+	defer inputFile.Close()
+
+	result := getResult(inputFile)
+
+	log.Printf("Final result: %d", result)
+	log.Printf("Execution took %s", time.Since(start))
+}
