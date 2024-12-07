3c153196104cf4a01862661e46e51265914dacab
Antoine CHABERT <chabert.antoine@gmail.com>
2024-12-07 13:42:21 +0100

    2024-Day-07 Part 1

---
+++ 2024/day07/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func getTestValueIfValid(testNum int64, values []int64, currentResult int64) int64 {
+	if currentResult > testNum {
+		return 0
+	}
+	if len(values) == 0 {
+		if testNum == currentResult {
+			return testNum
+		}
+		return 0
+	}
+	addition := getTestValueIfValid(testNum, values[1:], currentResult+values[0])
+	if addition > 0 {
+		return addition
+	}
+	if currentResult == 0 {
+		return getTestValueIfValid(testNum, values[1:], values[0])
+	}
+	return getTestValueIfValid(testNum, values[1:], currentResult*values[0])
+}
+
+func parseLine(line string) int64 {
+	parts := strings.Split(line, ": ")
+	if len(parts) != 2 {
+		log.Fatalf("Invalid line: %s", line)
+	}
+	testNum, errTestNum := strconv.ParseInt(parts[0], 10, 64)
+	if errTestNum != nil {
+		log.Fatalf("Invalid test number '%s': %v", parts[0], errTestNum)
+	}
+	valuesStr := strings.Split(parts[1], " ")
+	var values []int64
+	for _, valueStr := range valuesStr {
+		value, errValue := strconv.ParseInt(valueStr, 10, 64)
+		if errValue != nil {
+			log.Fatalf("Invalid value '%s': %v", valueStr, errValue)
+		}
+		values = append(values, value)
+	}
+	return getTestValueIfValid(testNum, values, 0)
+}
+
+func parseInput(input io.Reader) int64 {
+	scanner := bufio.NewScanner(input)
+
+	var result int64
+	for scanner.Scan() {
+		result += parseLine(scanner.Text())
+	}
+
+	if errScanningFile := scanner.Err(); errScanningFile != nil {
+		log.Fatalf("Unable to scan the input file correctly: %v", errScanningFile)
+	}
+
+	return result
+}
+
+func getResult(input io.Reader) int64 {
+	return parseInput(input)
+}
+
+func loadFile() *os.File {
+	inputFile, errOpeningFile := os.Open("./input.txt")
+	if errOpeningFile != nil {
+		log.Fatalf("Unable to open input file: %v", errOpeningFile)
+	}
+	return inputFile
+}
+
+func main() {
+	start := time.Now()
+	inputFile := loadFile()
+	defer inputFile.Close()
+
+	result := getResult(inputFile)
+
+	log.Printf("Final result: %d", result)
+	log.Printf("Execution took %s", time.Since(start))
+}
---
+++ 2024/day07/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+const testingInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
+
+const testingExpectedResult = 3749
+
+func TestGetResults(t *testing.T) {
+	result := getResult(strings.NewReader(testingInput))
+	if result != testingExpectedResult {
+		t.Errorf("Expected result to be %d, got %d", testingExpectedResult, result)
+	}
+}
+
+func BenchmarkGetResult(b *testing.B) {
+	b.Run("small", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			getResult(strings.NewReader(testingInput))
+		}
+	})
+
+	b.Run("large", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			inputFile := loadFile()
+			getResult(inputFile)
+			inputFile.Close()
+		}
+	})
+}
